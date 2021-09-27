b0ff52b2cb9a945c95a56dbc690a5ac53a355f0b
taltla1028 <rainyw1028@naver.com>
2021-09-27 23:14:26 +0900

    js TIL Upload

---
+++ javascript/comment.js
@@ -0,0 +1,10 @@
+// 단일행 주석
+
+/*
+다중행 주석
+*/
+
+// let hello = "world";
+
+// 변수가 주석처리가 되어 log에 나오지않는다.
+console.log(hello);
---
+++ javascript/let.js
@@ -0,0 +1,8 @@
+let 안녕 ="하세요";
+let hi = "hello";
+let HI = "HELLO";
+
+
+console.log(안녕);
+console.log(hi);
+console.log(HI);
